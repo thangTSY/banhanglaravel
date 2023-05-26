6014b9f19409d2318733f27838be16f396f846d3
tsy-bnk <dahacta90@gmail.com>
2023-05-26 17:32:13 +0700

    create database

--- database/migrations/2023_05_26_083816_create_table_posts_table.php
+++ database/migrations/2023_05_26_083816_create_table_posts_table.php
@@ -12,10 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('hoten');
-            $table->string('sodienthoai');
-            $table->string('content');
+            $table->id();
             $table->timestamps();
         });
     }
